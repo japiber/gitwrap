aece2560861bbb4323ca2cecfdda355eb99dc9d1
Juan Antonio Piñero <japiber@gmail.com>
2024-12-18 07:33:35 +0100

    include a dry-run functionality

--- src/wrap_command.rs
+++ src/wrap_command.rs
@@ -3,6 +3,7 @@ use crate::WrapError;
 
 pub type FnOptionArg = Box<dyn Fn(&mut Command)>;
 
+/// A git command wrapper and manager
 pub struct WrapCommand {
     cmd: String,
     args: Vec<FnOptionArg>,
@@ -10,6 +11,8 @@ pub struct WrapCommand {
 }
 
 impl WrapCommand {
+    /// Creates a new git [cmd] command.
+    /// If [current_dir] is Some, the command working directory is set to it.
     pub fn new(cmd: &str, current_dir: Option<&str>) -> Self {
         let cd = if let Some(d) = current_dir  {
             Some(String::from(d))
@@ -23,10 +26,13 @@ impl WrapCommand {
         }
     }
 
+    /// Includes a new git command option
     pub fn option(&mut self, arg: FnOptionArg) {
         self.args.push(arg);
     }
 
+    /// Executes the git command as a child process, waiting for it to finish and collecting all of its output.
+    /// By default, stdout and stderr are captured (and used to provide the result string).
     pub fn execute(&self) -> Result<String, WrapError> {
         let mut cmd = self.command();
         match cmd.output() {
@@ -39,6 +45,17 @@ impl WrapCommand {
             }
             Err(_) => Err(WrapError::FailedExecuteProcess(format!("{:?}", cmd))),
         }
+    }
+
+    /// Dry-runs the git command returning the full command specification provided
+    pub fn dry_run(&self) -> Result<String, WrapError> {
+        let cmd = self.command();
+        let mut con : Vec<String> = Vec::new();
+        con.push(String::from(cmd.get_program().to_str().unwrap_or("")));
+        for arg in cmd.get_args() {
+            con.push(String::from(arg.to_str().unwrap_or("")));
+        }
+        Ok(con.join(" "))
     }
 
     fn command(&self) -> Command {
